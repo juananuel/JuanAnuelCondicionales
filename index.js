dd95dffdb666cf7127af4cce5ed8c9737346a614
Juan Anuel <juancarlosanuel@gmail.com>
2024-07-10 20:07:20 -0300

    ejercicios version 1

--- index.js
+++ index.js
@@ -4,42 +4,30 @@ let numero3 = 8
 
 let masChico
 
-let persona1 = {
-  nombre: "Juan Anuel",
-  edad: 40,
-  altura: 175,
-}
-
-let persona2 = {
-  nombre: "Liliana Gutierrez",
-  edad: 28,
-  altura: 178,
-}
-
 
 // 1 mayor
 if (numero1 > numero2) {
-  console.log("primer numero es mayor que el segundo")
+  console.log("Primer numero es mayor que el segundo")
 } else {
-  console.log("primer numero es menor o igual al segundo")
+  console.log("Primer numero es menor o igual al segundo")
 }
 
 
 // 2 iguales o diferentes
 if (numero1 == numero2) {
-  console.log("los 2 numeros son iguales")
+  console.log("Los 2 numeros son iguales")
 } else {
-  console.log("los 2 numeros son diferentes")
+  console.log("Los 2 numeros son diferentes")
 }
 
 
-// 3 grade o iguales
+// 3 grande o iguales
 if (numero1 > numero2) {
-  console.log("el primero numero es mas grande")
+  console.log("El primero numero es mas grande")
 } else if (numero1 < numero2) {
-  console.log("el segundo numero es mas grande")
+  console.log("El segundo numero es mas grande")
 } else {
-  console.log("los 2 numeros son iguales")
+  console.log("Los 2 numeros son iguales")
 }
 
 
@@ -56,6 +44,18 @@ console.log("El numero mas chico es: " + masChico)
 
 
 // 5 comparando objetos
+let persona1 = {
+  nombre: "Juan Anuel",
+  edad: 40,
+  altura: 176,
+}
+
+let persona2 = {
+  nombre: "Liliana Gutierrez",
+  edad: 38,
+  altura: 178,
+}
+
 if (persona1.altura > persona2.altura) {
   console.log(persona1.nombre + " tiene mas altura que " + persona2.nombre)
 } else if (persona1.altura < persona2.altura) {
@@ -77,108 +77,114 @@ if (persona1.edad > persona2.edad) {
 }
 
 
-// 6 conducir
+// 6 *** capacitado conducir *** 
+
 // let nombre = prompt("Ingrese su nombre: ")
 // let edad = parseInt(prompt("Ingrese su edad: "))
 // let altura = parseInt(prompt("Ingrese su altura: "))
 // let vision = parseInt(prompt("Ingrese su visión (evaluando de 1 a 10): "))
 
 // if (edad >= 18 && altura > 150 && vision >= 8) {
-//   console.log(nombre + " esta capacitado para conducir")
+//   alert(nombre + " esta capacitado para conducir")
 // } else {
-//   console.log(nombre + " no esta capacitado para conducir")
+//   alert(nombre + " no esta capacitado para conducir")
 // }
 
 
-// 7 pase
+// 7 *** pase vip o normal ***
+
 // let nombre = prompt("Ingrese su nombre: ")
 // let pase = prompt("Ingrese su pase (vip o normal):").toLowerCase()
 // let poseeEntrada = prompt("¿Posee entrada? (si o no)").toLowerCase() 
 
 // if (nombre.toLowerCase() === 'juan' || pase === 'vip') {
-//     console.log("Bienvenido " + nombre.toUpperCase())
+//   alert("Bienvenido " + nombre.toUpperCase())
 // } else if (poseeEntrada === 'si') {
 //     let usarEntrada = prompt("¿Desea utilizar su entrada? (si o no)").toLowerCase() 
 //     if (usarEntrada === 'si') {
-//         console.log("Bienvenido " + nombre.toUpperCase())
+//       alert("Bienvenido " + nombre.toUpperCase())
 //     } else {
-//         console.log("Gracias por usar nuestros servicios")
+//       alert("Gracias por usar nuestros servicios")
 //     }
 // } else {
 //     let deseaComprar = prompt("¿Desea comprar una entrada? (si o no)").toLowerCase() 
 //     if (deseaComprar === 'si') {
-//         let dineroDisponible = parseFloat(prompt("Ingrese el dinero disponible: "))
-//         if (dineroDisponible >= 1000) {
-//             console.log("Venta de entrada realizada. Bienvenido " + nombre.toUpperCase())
-//         } else {
-//             console.log("Lo siento, no tienes suficiente dinero para comprar una entrada.")
-//         }
+//       let dineroDisponible = parseFloat(prompt("Ingrese el dinero disponible: "))
+//       if (dineroDisponible >= 1000) {
+//         alert("Venta de entrada positivo. Bienvenido " + nombre.toUpperCase())
+//       } else {
+//         alert("Lo siento, no posee suficiente dinero para comprar una entrada.")
+//       }
 //     } else {
-//         console.log("Gracias por usar nuestros servicios")
+//       alert("Gracias por usar nuestros servicios")
 //     }
 // }
 
 
-// 8 
+// 8 *** juego adivinanza ***
+
 // let numeroIncognita = Math.floor(Math.random() * 10) + 1;
-let numeroIncognita = 5;
-let numeroIngresado;
 
-numeroIngresado = parseInt(prompt("Adivina el número (entre 1 y 10):"))
-if (numeroIngresado === numeroIncognita) {
-  console.log("¡Ganaste, has adivinado el número!")
-} else {
-    if (numeroIngresado > numeroIncognita) {
-      console.log("El número ingresado es mayor, vuelve a intentarlo.")
-    } else {
-      console.log("El número ingresado es menor, vuelve a intentarlo.")
-    }
+// let numeroIncognita = 5;
+// let numeroIngresado;
 
-    numeroIngresado = parseInt(prompt("Adivina el número (entre 1 y 10):"))
-    if (numeroIngresado === numeroIncognita) {
-      console.log("¡Ganaste, has adivinado el número!")
-    } else {
-      if (numeroIngresado > numeroIncognita) {
-        console.log("El número ingresado es mayor, vuelve a intentarlo.")
-      } else {
-        console.log("El número ingresado es menor, vuelve a intentarlo.")
-      }
-
-      numeroIngresado = parseInt(prompt("Adivina el número (entre 1 y 10):"))
-      if (numeroIngresado === numeroIncognita) {
-        console.log("¡Ganaste, has adivinado el número!")
-      } else {
-        if (numeroIngresado > numeroIncognita) {
-          console.log("El número ingresado es mayor.")
-      } else {
-        console.log("El número ingresado es menor.")
-      }
-        console.log("Lo siento, se han agotado tus intentos.")
-      }
-    }
-}
-
-
-
-// 9 edad
-// let edad = parseInt(prompt("Ingrese su edad: "))
-
-// if (edad >= 0 && edad <= 12) {
-//     console.log("Eres un infante.")
-// } else if (edad >= 13 && edad <= 18) {
-//     console.log("Eres un adolescente.")
-// } else if (edad >= 19 && edad <= 45) {
-//     console.log("Eres un mayor joven.")
-// } else if (edad > 45 && edad <= 100) {
-//     console.log("Eres un anciano.")
-// } else if (edad > 100) {
-//     console.log("¿En realidad tienes esa edad? xD")
+// numeroIngresado = parseInt(prompt("Adivina el número (entre 1 y 10):"))
+// if (numeroIngresado === numeroIncognita) {
+//   alert("¡Ganaste, has adivinado el número!")
 // } else {
-//     console.log("Edad ingresada no válida.")
+//     if (numeroIngresado > numeroIncognita) {
+//       alert("El número ingresado es mayor, vuelve a intentarlo.")
+//     } else {
+//       alert("El número ingresado es menor, vuelve a intentarlo.")
+//     }
+
+//     numeroIngresado = parseInt(prompt("Adivina el número (entre 1 y 10):"))
+//     if (numeroIngresado === numeroIncognita) {
+//       alert("¡Ganaste, has adivinado el número!")
+//     } else {
+//       if (numeroIngresado > numeroIncognita) {
+//         alert("El número ingresado es mayor, vuelve a intentarlo.")
+//       } else {
+//         alert("El número ingresado es menor, vuelve a intentarlo.")
+//       }
+
+//       numeroIngresado = parseInt(prompt("Adivina el número (entre 1 y 10):"))
+//       if (numeroIngresado === numeroIncognita) {
+//         alert("¡Ganaste, has adivinado el número!")
+//       } else {
+//         if (numeroIngresado > numeroIncognita) {
+//           alert("El número ingresado es mayor.")
+//       } else {
+//         alert("El número ingresado es menor.")
+//       }
+//         alert("Lo siento, se han agotado tus intentos.")
+//       }
+//     }
 // }
 
 
-// 10 piedra papel tijera
+
+// 9 *** edad ***
+
+// let edad = parseInt(prompt("Ingrese su edad: "))
+
+// if (edad >= 0 && edad <= 12) {
+//   alert("Eres un infante.")
+// } else if (edad >= 13 && edad <= 18) {
+//   alert("Eres un adolescente.")
+// } else if (edad >= 19 && edad <= 45) {
+//   alert("Eres un mayor joven.")
+// } else if (edad > 45 && edad <= 100) {
+//   alert("Eres un anciano.")
+// } else if (edad > 100) {
+//   alert("¿En realidad tienes esa edad? xD")
+// } else {
+//   alert("Edad ingresada no válida.")
+// }
+
+
+// 10 *** piedra papel tijera ***
+
 // let jugador1 = prompt("Jugador 1, ingrese PIEDRA, PAPEL o TIJERAS: ").toUpperCase()
 // let jugador2 = prompt("Jugador 2, ingrese PIEDRA, PAPEL o TIJERAS: ").toUpperCase()
 
@@ -198,7 +204,8 @@ if (numeroIngresado === numeroIncognita) {
 // }
 
 
-// 11 color ingresado
+// 11 *** color ingresado ***
+
 // let color = prompt("Ingresa un color: ").toLocaleLowerCase()
 
 // switch(color){
@@ -228,10 +235,11 @@ if (numeroIngresado === numeroIncognita) {
 // }
 
 
-// 12 
+// *** 12 seleccion operacion ***
+
 // let valor1 = parseFloat(prompt("Ingrese un primer numero entero: "))
 // let valor2 = parseFloat(prompt("Ingrese un segundo numero entero: "))
-// let operacion = parseInt(prompt("Ingrese una de las opciones (1 - 4): \n1 - Suma \n2 - Resta \n3 - Multiplicación \n4 - División \n"))
+// let operacion = parseInt(prompt("Ingrese una de las opciones en número (1 - 4): \n1 - Suma \n2 - Resta \n3 - Multiplicación \n4 - División \n"))
 
 // let suma = valor1+valor2
 // let resta = valor1-valor2
@@ -260,7 +268,8 @@ if (numeroIngresado === numeroIncognita) {
 // }
 
 
-// 13 console table
+// 13 *** console table ***
+
 // let nombre = prompt("Ingrese su nombre: ")
 // let apellido = prompt("Ingrese su apellido: ")
 // let nacionalidad = prompt("Ingrese su nacionalidad: ")
